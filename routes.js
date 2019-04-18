27049c32d6f28adcaca0fb14684bc46d9eca21fd
Cian O'Gralaigh <ihatecian@gmail.com>
2019-04-18 14:33:03 +0100

    Add route, form and output functionality

--- routes.js
+++ routes.js
@@ -6,9 +6,24 @@ const requestHandler = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Hello User</title></head>');
-    res.write('<ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>');
+    res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul>');
+    res.write(
+      '<form method="POST" action="/create-user"><input name="username" type="text" /><button type="submit">Submit</button></form></body>'
+    );
     res.write('</html>');
     return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
+    req.on('data', chunk =>
+      console.log(`Your username is: ${chunk.toString().split('=')[1]}`)
+    );
+
+    req.on('end', () => {
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
   }
 };
 
